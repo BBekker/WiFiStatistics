6e6b3382eba74ab2f478b9841024cb011830fd11
Bernard Bekker <bernard@bernardbekker.nl>
2018-02-23 16:37:15 +0100

    add capture code

--- analyse.py
+++ analyse.py
@@ -1,8 +1,23 @@
 import pyshark
 import sys
+import csv
+import datetime
 
-capture = pyshark.FileCapture(sys.argv[1]);
+capture = pyshark.LiveCapture(sys.argv[1], capture_filter="wlan.fc.type eq 2", monitor_mode=True);
 
-for cap in capture:
-	if cap.wlan.fcs_status == 0:
-		print(cap)
+
+
+with open(datetime.now().isoformat() + '.csv', 'wb') as csvfile:
+        fieldnames = ["timestamp", "channel", "signal", "datarate", "phy", "crc", "len"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for cap in capture.sniff_continously():
+                row = {"timestamp" : frame.type,
+                       "channel" : wlan_radio.channel,
+                       "signal" : wlan_radio.signal_dbm,
+                       "datarate" : wlan_radio.data_rate,
+                       "phy" : wlan_radio.phy,
+                       "crc" : wlan.fc.status,
+                       "len" : frame.len}
+                writer.writerow(row)
+                       
