221d0b57b7dfa6d4466d57f7193633840f4f06ed
Bernard Bekker <bernard@bernardbekker.nl>
2018-03-06 22:36:42 +0100

    cleaned up files, a lot of report, sources, figures

--- analyse.py
+++ analyse.py
@@ -8,7 +8,10 @@ import math
 
 argv = sys.argv
 data = pandas.read_csv(argv[1], nrows = int(argv[2]), header=None, names=['timestamp','chan','signal','datarate','phy','crc','length'])
+data['dataratefloat'] = data[['datarate']].applymap(lambda x: float(str(x).split(',')[0]))
 
+print(data)
+#print(data.groupby('dataratefloat').count())
 #CRC vs signal strength
 #
 # bysignal = data.groupby('signal')['crc'].mean().rolling(1).mean()
@@ -58,12 +61,39 @@ def graph58ghz(data):
     fig.set_ylabel("Percentage of packets correctly received", color='b')
     plt.show()
 
+def graph58ghz144mbps(data):
+
+    filter = data['chan'].apply(int) > 15
+    bysignal = data[filter][data['dataratefloat'] >= 80].groupby('signal')['crc'].mean().rolling(1).mean()
+    samples = data[filter][data['dataratefloat'] >= 80].groupby('signal')['crc'].count()
+    fig = bysignal[samples > 10].plot(title='5.8 GHz', color='b', style='.-')
+
+    fig2 = samples[samples > 10].plot(ax=fig, secondary_y=True, color='y', style='.-')
+    fig2.set_ylabel("samples", color='y')
+    fig.set_xlabel("SSI signal strength [dBm]")
+    fig.set_ylabel("Percentage of packets correctly received", color='b')
+    plt.show()
+
+
+def graph24ghz72mbps(data):
+
+    filter = data['chan'].apply(int) < 15
+    bysignal = data[filter][data['dataratefloat'] == 24.0 ].groupby('signal')['crc'].mean().rolling(1).mean()
+    samples = data[filter][data['dataratefloat'] == 24.0 ].groupby('signal')['crc'].count()
+    fig = bysignal[samples > 10].plot(title='correctly received packets at 2.8 GHz, 72.2Mbps', color='b', style='.-')
+
+    fig2 = samples[samples > 10].plot(ax=fig, secondary_y=True, color='y', style='.-')
+    fig2.set_ylabel("samples", color='y')
+    fig.set_xlabel("SSI signal strength [dBm]")
+    fig.set_ylabel("Percentage of packets correctly received", color='b')
+    plt.show()
 
 def graph24ghz(data):
+
     filter = data['chan'].apply(int) < 15
     bysignal = data[filter].groupby('signal')['crc'].mean().rolling(1).mean()
     samples = data[filter].groupby('signal')['crc'].count()
-    fig = bysignal[samples > 10].plot(title='5.8 GHz', color='b', style='.-')
+    fig = bysignal[samples > 10].plot(title='correctly received packets at 2.8 GHz, 72.2Mbps', color='b', style='.-')
 
     fig2 = samples[samples > 10].plot(ax=fig, secondary_y=True, color='y', style='.-')
     fig2.set_ylabel("samples", color='y')
@@ -74,14 +104,14 @@ def graph24ghz(data):
 def graphlength(data):
 
     samples = data.groupby('length')['crc'].count()
-    bylength = data.groupby('length')['crc'].mean()[samples>5].rolling(5).mean()
+    bylength = data.groupby('length')['crc'].mean()[samples>4].rolling(5).mean()
     plt.figure()
-    fig =bylength[samples>2].plot(title='percentage of correctly received packets vs packet length')
-    bylength[samples>2].rolling(20).mean().plot()
+    fig =bylength[samples>4].plot(title='percentage of correctly received packets vs packet length')
+    bylength[samples>4].rolling(20).mean().plot()
     #samples[samples > 10].plot(ax=fig, secondary_y=True)
     #fig.right_ax.set_yscale('log')
     l = np.arange(150,1700,1)
-    plt.plot(l, 0.99 - 0.0003*l)
+    plt.plot(l, (1 - (0.001**l)))
 
     plt.show()
 
@@ -94,7 +124,24 @@ def graphlength(data):
 
     # print(resampled)
     # print(sm.OLS(nf['crc'],nf['length']).fit().summary())
-    
+
+def graphairtime(data):
+    data['airtime'] = (data['length'] * 8) / data['dataratefloat']
+
+    samples = data.groupby('airtime')['crc'].count()
+    bylength = data.groupby('airtime')['crc'].mean()[samples>4].rolling(5).mean()
+    plt.figure()
+    fig =bylength[samples>4].plot(title='percentage of correctly received packets vs packet airtime',color='gray')
+    bylength[samples>4].rolling(50).mean().plot(color='black')
+    fig.set_xlabel("airtime [uS]")
+    fig.set_ylabel("non-crc error %")
+    #samples[samples > 10].plot(ax=fig, secondary_y=True)
+    #fig.right_ax.set_yscale('log')
+    #l = np.arange(150,1700,1)
+    #plt.plot(l, (1 - (0.001**l)))
+
+    plt.show()
+
 
 # lengths = data.groupby('length')['crc'].mean().to_frame().reset_index(level=0)
 # print(sm.OLS(lengths['length'], lengths['crc']).fit().summary())
@@ -118,4 +165,7 @@ def dataratevsstrength(data):
     fig.legend([fig,fig2],["all packets", "successful packets"])
     plt.show()
 
-graphlength(data)
+#graph24ghz72mbps(data)
+graphairtime(data)
+#print(data.groupby('chan')['crc'].mean())
+#print(data.groupby('chan')['crc'].count())
