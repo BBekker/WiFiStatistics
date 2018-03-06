8fa670264aef6afc1d2ac3a1f53701d45e6e650c
Bernard Bekker <bernard@bernardbekker.nl>
2018-03-06 15:41:06 +0100

    update report

--- analyse.py
+++ analyse.py
@@ -74,29 +74,48 @@ def graph24ghz(data):
 def graphlength(data):
 
     samples = data.groupby('length')['crc'].count()
-    bylength = data.groupby('length')['crc'].mean()[samples>5].rolling(20).mean()
+    bylength = data.groupby('length')['crc'].mean()[samples>5].rolling(5).mean()
     plt.figure()
-    fig =bylength.plot(title='percentage of correctly received packets vs packet length')
-    samples[samples > 10].plot(ax=fig, secondary_y=True)
-    fig.right_ax.set_yscale('log')
+    fig =bylength[samples>2].plot(title='percentage of correctly received packets vs packet length')
+    bylength[samples>2].rolling(20).mean().plot()
+    #samples[samples > 10].plot(ax=fig, secondary_y=True)
+    #fig.right_ax.set_yscale('log')
+    l = np.arange(150,1700,1)
+    plt.plot(l, 0.99 - 0.0003*l)
+
     plt.show()
+
+    # nf = bylength.to_frame().reset_index().dropna();
+
+    # newindex = pandas.RangeIndex(1,1618,1)
+
+    # resampled = nf.reindex('length',newindex)
+    # #resampled.interpolate('index',inplace=True)
+
+    # print(resampled)
+    # print(sm.OLS(nf['crc'],nf['length']).fit().summary())
+    
 
 # lengths = data.groupby('length')['crc'].mean().to_frame().reset_index(level=0)
 # print(sm.OLS(lengths['length'], lengths['crc']).fit().summary())
 # sm.graphics.plot_partregress('crc', 'length', ['crc','length'],data=lengths)
 # plt.show()
 #graphlength(data)
-data['dataratefloat'] = data[['datarate']].applymap(lambda x: float(x.split(',')[0]))
-print(data[data.crc == 1][['crc','signal','dataratefloat']].groupby('signal')['dataratefloat'].max().to_frame())
-fig = data[data.crc == 1][['crc','signal','dataratefloat']].groupby('signal')['dataratefloat'].mean().plot(color='y', style='.-')
-fig2 = data[['crc','signal','dataratefloat']].groupby('signal')['dataratefloat'].mean().plot(color='b', style='.-')
-data[['crc','signal','dataratefloat']].groupby('signal')['dataratefloat'].max().plot(color='b', style='.-')
-data[data.crc == 1][['crc','signal','dataratefloat']].groupby('signal')['dataratefloat'].max().plot(color='y', style='.-')
 
-data[['crc','signal','dataratefloat']].groupby('signal')['dataratefloat'].min().plot(color='b', style='.-')
-data[data.crc == 1][['crc','signal','dataratefloat']].groupby('signal')['dataratefloat'].min().plot(color='y', style='.-')
+def dataratevsstrength(data):
+    data['dataratefloat'] = data[['datarate']].applymap(lambda x: float(x.split(',')[0]))
+    print(data[data.crc == 1][['crc','signal','dataratefloat']].groupby('signal')['dataratefloat'].max().to_frame())
+    fig = data[data.crc == 1][['crc','signal','dataratefloat']].groupby('signal')['dataratefloat'].mean().plot(color='y', style='.-')
+    fig2 = data[['crc','signal','dataratefloat']].groupby('signal')['dataratefloat'].mean().plot(color='b', style='.-')
+    data[['crc','signal','dataratefloat']].groupby('signal')['dataratefloat'].max().plot(color='b', style='.-')
+    data[data.crc == 1][['crc','signal','dataratefloat']].groupby('signal')['dataratefloat'].max().plot(color='y', style='.-')
 
-fig.set_ylabel("datarate [Mbps]")
-fig.set_xlabel("SSI signal strength [dBm]")
-fig.legend([fig,fig2],["all packets", "successful packets"])
-plt.show()
+    data[['crc','signal','dataratefloat']].groupby('signal')['dataratefloat'].min().plot(color='b', style='.-')
+    data[data.crc == 1][['crc','signal','dataratefloat']].groupby('signal')['dataratefloat'].min().plot(color='y', style='.-')
+
+    fig.set_ylabel("datarate [Mbps]")
+    fig.set_xlabel("SSI signal strength [dBm]")
+    fig.legend([fig,fig2],["all packets", "successful packets"])
+    plt.show()
+
+graphlength(data)
